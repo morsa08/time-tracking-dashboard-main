55f5096ddff4095f4e917c26c5e26dcfa3dad055
morsa08 <86577165+morsa08@users.noreply.github.com>
2022-12-31 15:15:57 -0600

    Json update
    Removed brackets

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-fetch('./data.json')
-    .then((response) => response.json())
-    .then((json)=> console.log(json));
+let newList;
+
+fetchList(function(list){
+    console.log(list);
+    newList = list;
+    result = list.filter(obj=> {obj.title == "Work"});
+    console.log(result[0]);
+});
+
+async function fetchList(callback){
+
+    const res = await fetch('https://morsa08.github.io/time-tracking-dashboard-main/data.json');
+
+    const list = await res.json();
+    callback(list);
+}
+
+let work = $(".work")[0];
+
+
